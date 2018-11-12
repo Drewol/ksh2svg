dcc8f984dcaabd7ba584162c34b3de197809a29c
Drewol <emildraws@gmail.com>
2018-11-12 23:57:43 +0100

    Make whitespace consistent

--- ksh2svg.py
+++ ksh2svg.py
@@ -54,60 +54,60 @@ def measures_to_length(measures):
         result += m[0]
     return result
 
-		
+        
 def map_laser(l):
-	result = LASER_VALUES.index(l)
-	result /= 50
-	return result
-		
+    result = LASER_VALUES.index(l)
+    result /= 50
+    return result
+        
 def get_next_laser(pos, datalines, measure_numbers, laser):
-	counter = 1
-	for line in datalines:
-		to_add = pos_to_measure(pos,measure_numbers)[1]
-		l = line[8 + laser]
-		if l in LASER_VALUES:
-			return [map_laser(l), pos, counter]
-		elif l == '-':
-			return None
-		pos += to_add
-		counter += 1
-	return None
-		
-def	draw_measure_lines(svg, measures):
-	height = measures_to_length(measures)
-	x0 = MEASURE_WIDTH / 2.0 + LASER_WIDTH + 1
-	x1 = x0 + MEASURE_WIDTH - 2 * LASER_WIDTH - 2
-	text_g = svg.g(class_="measure_numbering")
-	lines_g = svg.g(class_="measure_lines")
-	svg.add(text_g)
-	svg.add(lines_g)
-	pos = 0
-	index = 1
-	for m in measures:
-		y = height - pos
-		lines_g.add(svg.line((x0, y), (x1,y), stroke=svgwrite.rgb(100,100,100, '%')))
-		t = "%02d" % index
-		text_g.add(svg.text(t, insert = (x0 - 7 * len(t) - LASER_WIDTH - 1, y)))
-		for i in range(1, m[2][0]):
-			y_c = y - (i / m[2][1]) * 192.0
-			lines_g.add(svg.line((x0, y_c), (x1,y_c), stroke=svgwrite.rgb(25,25,25, '%')))
-		pos = pos + m[0]
-		index += 1
-	return svg
+    counter = 1
+    for line in datalines:
+        to_add = pos_to_measure(pos,measure_numbers)[1]
+        l = line[8 + laser]
+        if l in LASER_VALUES:
+            return [map_laser(l), pos, counter]
+        elif l == '-':
+            return None
+        pos += to_add
+        counter += 1
+    return None
+        
+def draw_measure_lines(svg, measures):
+    height = measures_to_length(measures)
+    x0 = MEASURE_WIDTH / 2.0 + LASER_WIDTH + 1
+    x1 = x0 + MEASURE_WIDTH - 2 * LASER_WIDTH - 2
+    text_g = svg.g(class_="measure_numbering")
+    lines_g = svg.g(class_="measure_lines")
+    svg.add(text_g)
+    svg.add(lines_g)
+    pos = 0
+    index = 1
+    for m in measures:
+        y = height - pos
+        lines_g.add(svg.line((x0, y), (x1,y), stroke=svgwrite.rgb(100,100,100, '%')))
+        t = "%02d" % index
+        text_g.add(svg.text(t, insert = (x0 - 7 * len(t) - LASER_WIDTH - 1, y)))
+        for i in range(1, m[2][0]):
+            y_c = y - (i / m[2][1]) * 192.0
+            lines_g.add(svg.line((x0, y_c), (x1,y_c), stroke=svgwrite.rgb(25,25,25, '%')))
+        pos = pos + m[0]
+        index += 1
+    return svg
 
 def draw_bpm_text(svg, filename, measure_numbers):
-	pos = 0
-	height = measures_to_length(measure_numbers)
-	g = svg.g(class_="bpm_text")
-	svg.add(g)
-	with open(filename, 'r') as f:
-		all_lines = f.readlines()
-		for line in all_lines:
-			if re.match("[0-2]{4}\\|.*", line) != None:
-				pos += pos_to_measure(pos,measure_numbers)[1]
-			elif line.startswith("t=") and '-' not in line:
-				g.add(svg.text(line[2:], insert = (MEASURE_WIDTH * 1.5, height - pos)))
-	return svg
+    pos = 0
+    height = measures_to_length(measure_numbers)
+    g = svg.g(class_="bpm_text")
+    svg.add(g)
+    with open(filename, 'r') as f:
+        all_lines = f.readlines()
+        for line in all_lines:
+            if re.match("[0-2]{4}\\|.*", line) != None:
+                pos += pos_to_measure(pos,measure_numbers)[1]
+            elif line.startswith("t=") and '-' not in line:
+                g.add(svg.text(line[2:], insert = (MEASURE_WIDTH * 1.5, height - pos)))
+    return svg
 
 def get_expand_ranges(filename, measures):
     pos = 0
@@ -130,168 +130,168 @@ def get_expand_ranges(filename, measures):
     return result
 
 def main(filename, savePath):
-	pos = 0
-	measure_numbers = get_measure_numbers(filename)
-	expand_ranges = get_expand_ranges(filename, measure_numbers)
-	line_index = 0
-	height = measures_to_length(measure_numbers)
-	
-	output = svgwrite.Drawing(savePath, size=(u'%dpx' % (MEASURE_WIDTH * 2), u'%dpx' % height), profile='full')
-	css = """
+    pos = 0
+    measure_numbers = get_measure_numbers(filename)
+    expand_ranges = get_expand_ranges(filename, measure_numbers)
+    line_index = 0
+    height = measures_to_length(measure_numbers)
+
+    output = svgwrite.Drawing(savePath, size=(u'%dpx' % (MEASURE_WIDTH * 2), u'%dpx' % height), profile='full')
+    css = """
 text
 {
-	font-family: Noto Mono, monospace;
-	font-size: 10px;
+    font-family: Noto Mono, monospace;
+    font-size: 10px;
 }
 .bpm_text
 {
-	font-size: 8px;
-	fill: lime;
+    font-size: 8px;
+    fill: lime;
 }
-	"""
-	output.defs.add(output.style(css))
-	lane_x = MEASURE_WIDTH / 2.0
-	lane_w = MEASURE_WIDTH
-	lane_y = 0
-	lane_h = height
-	measure_g = output.g(class_="measure")
-	measure_g.add(output.rect((lane_x,lane_y),(lane_w,lane_h), fill=svgwrite.rgb(5,5,5,'%')))
+    """
+    output.defs.add(output.style(css))
+    lane_x = MEASURE_WIDTH / 2.0
+    lane_w = MEASURE_WIDTH
+    lane_y = 0
+    lane_h = height
+    measure_g = output.g(class_="measure")
+    measure_g.add(output.rect((lane_x,lane_y),(lane_w,lane_h), fill=svgwrite.rgb(5,5,5,'%')))
 
-	#Left Laser lane
-	measure_g.add(output.rect((lane_x,lane_y), (LASER_WIDTH, lane_h), fill=svgwrite.rgb(5,10,25,'%')))
-	lane_x = lane_x + LASER_WIDTH
-	measure_g.add(output.rect((lane_x,lane_y), (1, lane_h), fill=svgwrite.rgb(40,40,40,'%')))
-	lane_x = lane_x + 1
-	#Button lanes
-	for i in range(4):
-		lane_x = lane_x + BUTTON_WIDTH
-		measure_g.add(output.rect((lane_x,lane_y), (1, lane_h), fill=svgwrite.rgb(40,40,40,'%')))
-		lane_x = lane_x + 1
-	
-	#Right Laser lane
-	measure_g.add(output.rect((lane_x,lane_y), (LASER_WIDTH, lane_h), fill=svgwrite.rgb(25,5,10,'%')))
-	output.add(measure_g)
-	lane_x = MEASURE_WIDTH / 2.0	
-	
-	output = draw_measure_lines(output, measure_numbers)
-	output = draw_bpm_text(output, filename, measure_numbers)
-	
-	lasers = (output.g(class_="lasers_l"), output.g(class_="lasers_r"))
-	bt_holds = output.g(class_="bt_holds")
-	bt_chips = output.g(class_="bt_chips")
-	fx_holds = output.g(class_="fx_holds")
-	fx_chips = output.g(class_="fx_chips")
-	
-	fx_hold_active = [(False, 0), (False, 0)]
-	bt_hold_active = [(False, 0), (False, 0), (False, 0), (False, 0)]
-	
-	with open(filename, 'r') as f:
-		all_lines = f.read()
-		all_datalines = re.findall("[0-2]{4}\\|.*", all_lines)
-		for line in all_datalines:
-			to_add = pos_to_measure(pos,measure_numbers)[1]
-			
-			#BTs
-			for i in range(4):
-				l = line[i]
-				bt_x = lane_x + LASER_WIDTH + 1 + (BUTTON_WIDTH + 1) * i
+    #Left Laser lane
+    measure_g.add(output.rect((lane_x,lane_y), (LASER_WIDTH, lane_h), fill=svgwrite.rgb(5,10,25,'%')))
+    lane_x = lane_x + LASER_WIDTH
+    measure_g.add(output.rect((lane_x,lane_y), (1, lane_h), fill=svgwrite.rgb(40,40,40,'%')))
+    lane_x = lane_x + 1
+    #Button lanes
+    for i in range(4):
+        lane_x = lane_x + BUTTON_WIDTH
+        measure_g.add(output.rect((lane_x,lane_y), (1, lane_h), fill=svgwrite.rgb(40,40,40,'%')))
+        lane_x = lane_x + 1
+    
+    #Right Laser lane
+    measure_g.add(output.rect((lane_x,lane_y), (LASER_WIDTH, lane_h), fill=svgwrite.rgb(25,5,10,'%')))
+    output.add(measure_g)
+    lane_x = MEASURE_WIDTH / 2.0    
+    
+    output = draw_measure_lines(output, measure_numbers)
+    output = draw_bpm_text(output, filename, measure_numbers)
+    
+    lasers = (output.g(class_="lasers_l"), output.g(class_="lasers_r"))
+    bt_holds = output.g(class_="bt_holds")
+    bt_chips = output.g(class_="bt_chips")
+    fx_holds = output.g(class_="fx_holds")
+    fx_chips = output.g(class_="fx_chips")
+    
+    fx_hold_active = [(False, 0), (False, 0)]
+    bt_hold_active = [(False, 0), (False, 0), (False, 0), (False, 0)]
+    
+    with open(filename, 'r') as f:
+        all_lines = f.read()
+        all_datalines = re.findall("[0-2]{4}\\|.*", all_lines)
+        for line in all_datalines:
+            to_add = pos_to_measure(pos,measure_numbers)[1]
+            
+            #BTs
+            for i in range(4):
+                l = line[i]
+                bt_x = lane_x + LASER_WIDTH + 1 + (BUTTON_WIDTH + 1) * i
 
-				if (l == '0') and bt_hold_active[i][0]: # End Hold
-					bt_h = pos - bt_hold_active[i][1]
-					bt_y = height - pos
-					bt_holds.add(output.rect((bt_x,bt_y),(BUTTON_WIDTH, bt_h), fill=BT_COLOR))
-					bt_hold_active[i] = (False, 0)
-				elif (l == '2') and bt_hold_active[i][0] == False: # Start Hold
-					bt_hold_active[i] = (True, pos)
-								
-				if l == '1': # BT Chip
-					bt_y = height - pos - CHIP_HEIGHT
-					bt_chips.add(output.rect((bt_x,bt_y),(BUTTON_WIDTH, CHIP_HEIGHT), fill=BT_COLOR))			
-				
-			#FXs
-			for i in range(2):
-				l = line[5 + i]
-				fx_x = lane_x + LASER_WIDTH + 1 + (2 * BUTTON_WIDTH + 2) * i
+                if (l == '0') and bt_hold_active[i][0]: # End Hold
+                    bt_h = pos - bt_hold_active[i][1]
+                    bt_y = height - pos
+                    bt_holds.add(output.rect((bt_x,bt_y),(BUTTON_WIDTH, bt_h), fill=BT_COLOR))
+                    bt_hold_active[i] = (False, 0)
+                elif (l == '2') and bt_hold_active[i][0] == False: # Start Hold
+                    bt_hold_active[i] = (True, pos)
+                                
+                if l == '1': # BT Chip
+                    bt_y = height - pos - CHIP_HEIGHT
+                    bt_chips.add(output.rect((bt_x,bt_y),(BUTTON_WIDTH, CHIP_HEIGHT), fill=BT_COLOR))            
+                
+            #FXs
+            for i in range(2):
+                l = line[5 + i]
+                fx_x = lane_x + LASER_WIDTH + 1 + (2 * BUTTON_WIDTH + 2) * i
 
-				if (l == '2' or l == '0') and fx_hold_active[i][0]: # End Hold
-					fx_h = pos - fx_hold_active[i][1]
-					fx_y = height - pos
-					fx_holds.add(output.rect((fx_x,fx_y),(FX_WIDTH, fx_h), fill=FX_COLOR, fill_opacity=FX_OPACITY))
-					fx_hold_active[i] = (False, 0)
+                if (l == '2' or l == '0') and fx_hold_active[i][0]: # End Hold
+                    fx_h = pos - fx_hold_active[i][1]
+                    fx_y = height - pos
+                    fx_holds.add(output.rect((fx_x,fx_y),(FX_WIDTH, fx_h), fill=FX_COLOR, fill_opacity=FX_OPACITY))
+                    fx_hold_active[i] = (False, 0)
 
-				elif (l != '0' and l != '2') and fx_hold_active[i][0] == False: # Start Hold
-					fx_hold_active[i] = (True, pos)
-				
-				if l == '2': # FX Chip
-					fx_y = height - pos - CHIP_HEIGHT
-					fx_chips.add(output.rect((fx_x,fx_y),(FX_WIDTH, CHIP_HEIGHT), fill=FX_COLOR))			
+                elif (l != '0' and l != '2') and fx_hold_active[i][0] == False: # Start Hold
+                    fx_hold_active[i] = (True, pos)
+                
+                if l == '2': # FX Chip
+                    fx_y = height - pos - CHIP_HEIGHT
+                    fx_chips.add(output.rect((fx_x,fx_y),(FX_WIDTH, CHIP_HEIGHT), fill=FX_COLOR))            
 
-			
-			#Lasers
-			for i in range(2):
-				l = line[8 + i]
-				if l == '-':
-					continue
-				if l in LASER_VALUES:
-					curr_laser = [map_laser(l), pos]
-					next_laser = get_next_laser(pos + to_add, all_datalines[line_index + 1:], measure_numbers, i)
-					if next_laser != None:
-						is_expanded = False
-						for r in expand_ranges[i]:
-							if curr_laser[1] > r[1]:
-								continue
-							if next_laser[1] < r[0]:
-								break
-							if curr_laser[1] >= r[0] and next_laser[1] <= r[1]:
-								is_expanded = True
-								break
-							
-						
-						duration = next_laser[1] - curr_laser[1]
-						l_x0 = next_laser[0] * (MEASURE_WIDTH - LASER_WIDTH) + LASER_WIDTH * 0.5
-						l_x1 = curr_laser[0] * (MEASURE_WIDTH - LASER_WIDTH) + LASER_WIDTH * 0.5
-						if is_expanded:
-							l_x0 *= 2
-							l_x1 *= 2
-						else:
-							l_x0 += lane_x
-							l_x1 += lane_x
-						if duration > 6 and next_laser[2] > 1:
-							l_y0 = height - next_laser[1]
-							l_y1 = height - pos
-							l_points = [(l_x0 - LASER_WIDTH * 0.5, l_y0), 
-													(l_x0 + LASER_WIDTH * 0.5, l_y0),
-													(l_x1 + LASER_WIDTH * 0.5, l_y1), 
-													(l_x1 - LASER_WIDTH * 0.5, l_y1)]
-							lasers[i].add(output.polygon(l_points, fill=LASER_COLORS[i], fill_opacity=LASER_OPACITY))
-						else: # Slam
-							l_y = height - next_laser[1]
-							l_h = duration
-							l_x = min(l_x0, l_x1) - (LASER_WIDTH * 0.5)
-							l_w = abs(l_x0 - l_x1) + LASER_WIDTH
+            
+            #Lasers
+            for i in range(2):
+                l = line[8 + i]
+                if l == '-':
+                    continue
+                if l in LASER_VALUES:
+                    curr_laser = [map_laser(l), pos]
+                    next_laser = get_next_laser(pos + to_add, all_datalines[line_index + 1:], measure_numbers, i)
+                    if next_laser != None:
+                        is_expanded = False
+                        for r in expand_ranges[i]:
+                            if curr_laser[1] > r[1]:
+                                continue
+                            if next_laser[1] < r[0]:
+                                break
+                            if curr_laser[1] >= r[0] and next_laser[1] <= r[1]:
+                                is_expanded = True
+                                break
+                            
+                        
+                        duration = next_laser[1] - curr_laser[1]
+                        l_x0 = next_laser[0] * (MEASURE_WIDTH - LASER_WIDTH) + LASER_WIDTH * 0.5
+                        l_x1 = curr_laser[0] * (MEASURE_WIDTH - LASER_WIDTH) + LASER_WIDTH * 0.5
+                        if is_expanded:
+                            l_x0 *= 2
+                            l_x1 *= 2
+                        else:
+                            l_x0 += lane_x
+                            l_x1 += lane_x
+                        if duration > 6 and next_laser[2] > 1:
+                            l_y0 = height - next_laser[1]
+                            l_y1 = height - pos
+                            l_points = [(l_x0 - LASER_WIDTH * 0.5, l_y0), 
+                                        (l_x0 + LASER_WIDTH * 0.5, l_y0),
+                                        (l_x1 + LASER_WIDTH * 0.5, l_y1), 
+                                        (l_x1 - LASER_WIDTH * 0.5, l_y1)]
+                            lasers[i].add(output.polygon(l_points, fill=LASER_COLORS[i], fill_opacity=LASER_OPACITY))
+                        else: # Slam
+                            l_y = height - next_laser[1]
+                            l_h = duration
+                            l_x = min(l_x0, l_x1) - (LASER_WIDTH * 0.5)
+                            l_w = abs(l_x0 - l_x1) + LASER_WIDTH
 
-							lasers[i].add(output.rect((l_x,l_y),(l_w, l_h), fill=LASER_COLORS[i], fill_opacity=LASER_OPACITY))
-							if all_datalines[line_index + next_laser[2] + 1][8+i] == '-': # add end segmento
-								e_x = l_x0 - (LASER_WIDTH * 0.5);
-								lasers[i].add(output.rect((e_x, l_y - EXIT_HEIGHT), (LASER_WIDTH, EXIT_HEIGHT), fill=LASER_COLORS[i], fill_opacity=LASER_OPACITY))
-							if all_datalines[line_index - 1][8+i] == '-': # add start segment
-								e_x = l_x1 - (LASER_WIDTH * 0.5);
-								lasers[i].add(output.rect((e_x, l_y + duration), (LASER_WIDTH, ENTRY_HEIGHT), fill=LASER_COLORS[i], fill_opacity=LASER_OPACITY))
-								
-							
-			pos = pos + to_add
-			line_index += 1
-					
-	# add everything to separate lists and then
-	# add them to output here for proper layering
-	output.add(fx_holds)
-	output.add(bt_holds)
-	output.add(lasers[0])
-	output.add(lasers[1])
-	output.add(fx_chips)
-	output.add(bt_chips)
-	
-	output.save()
-	
+                            lasers[i].add(output.rect((l_x,l_y),(l_w, l_h), fill=LASER_COLORS[i], fill_opacity=LASER_OPACITY))
+                            if all_datalines[line_index + next_laser[2] + 1][8+i] == '-': # add end segmento
+                                e_x = l_x0 - (LASER_WIDTH * 0.5);
+                                lasers[i].add(output.rect((e_x, l_y - EXIT_HEIGHT), (LASER_WIDTH, EXIT_HEIGHT), fill=LASER_COLORS[i], fill_opacity=LASER_OPACITY))
+                            if all_datalines[line_index - 1][8+i] == '-': # add start segment
+                                e_x = l_x1 - (LASER_WIDTH * 0.5);
+                                lasers[i].add(output.rect((e_x, l_y + duration), (LASER_WIDTH, ENTRY_HEIGHT), fill=LASER_COLORS[i], fill_opacity=LASER_OPACITY))
+                                
+                            
+            pos = pos + to_add
+            line_index += 1
+                    
+    # add everything to separate lists and then
+    # add them to output here for proper layering
+    output.add(fx_holds)
+    output.add(bt_holds)
+    output.add(lasers[0])
+    output.add(lasers[1])
+    output.add(fx_chips)
+    output.add(bt_chips)
+    
+    output.save()
+    
 main(sys.argv[1], sys.argv[2])
 print("Finished!")
